4f6f42d5d424f935d6217fa85f1b43a094a2ca56
Wesley De Bolster <53346841+weslbo@users.noreply.github.com>
2026-04-20 08:57:32 +0200

    Remove optional initial catalog name support from setup script

--- Instructions/Labs/00-setup.sh
+++ Instructions/Labs/00-setup.sh
@@ -2,27 +2,8 @@
 
 # DP-750 Lab Setup Script
 # Creates an Azure Databricks Premium workspace in a randomly selected region.
-#
-# Usage: ./00-setup.sh [--initial-catalog-name <name>]
-#   --initial-catalog-name  Optional. Sets the initial name of the default Unity Catalog.
 
 set -e
-
-# Parse optional arguments
-INITIAL_CATALOG_NAME=""
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    --initial-catalog-name)
-      INITIAL_CATALOG_NAME="$2"
-      shift 2
-      ;;
-    *)
-      echo "Unknown argument: $1"
-      exit 1
-      ;;
-  esac
-done
 
 # Select a random Azure region that supports Azure Databricks
 REGIONS=( australiaeast australiasoutheast brazilsouth canadacentral canadaeast centralindia centralus eastasia eastus eastus2 francecentral germanywestcentral japaneast japanwest koreacentral northcentralus northeurope norwayeast southcentralus southeastasia southindia swedencentral switzerlandnorth uksouth ukwest westeurope westus westus2 westus3 )
@@ -43,18 +24,10 @@ az group create \
   --location $REGION
 
 echo "Creating Azure Databricks Premium workspace $WORKSPACE_NAME..."
-
-# Build optional default-catalog argument
-DEFAULT_CATALOG_ARG=""
-if [[ -n "$INITIAL_CATALOG_NAME" ]]; then
-  DEFAULT_CATALOG_ARG="--default-catalog initial-name=$INITIAL_CATALOG_NAME initial-type=UnityCatalog"
-fi
-
 az databricks workspace create \
   --resource-group $RESOURCE_GROUP \
   --name $WORKSPACE_NAME \
   --location $REGION \
-  --sku premium \
-  $DEFAULT_CATALOG_ARG
+  --sku premium
 
 echo "Installation done"
